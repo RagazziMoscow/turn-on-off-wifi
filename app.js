f0df97e11f22d2a6bb92cacca5e2e662b591d1c4
Ilya <iliya5376@mail.com>
2017-08-02 19:07:34 +0300

    refact(app.js) add events emitter for code refactoring

--- app.js
+++ app.js
@@ -1,37 +1,54 @@
+var events = require('events');
 var chalk = require('chalk');
 var telnet = require('telnet-client');
+var config = require('./config');
 var connection = new telnet();
+var wifi = new events.EventEmitter();
 
-var params = {
-  host: '192.168.1.1',
-  port: 23,
-  username: "admin",
-  password: "admin",
-  shellPrompt: '#',
-  timeout: 3500,
-  debug: true
-};
+var params = config.connection;
 
 connection.on('ready', function(prompt) {
   var cmd = 'ifconfig ra0 ' + process.argv[2];
   connection.exec(cmd, function(err, response) {
-    if (response) console.log(response);
+    //if (err) wifi.emit('error');
+
+    //if (response) wifi.emit(process.argv[2]);
+    wifi.emit(process.argv[2]);
   });
 });
 
 connection.on('timeout', function() {
-  console.log('socket timeout!')
+  //console.log('socket timeout!')
   connection.end();
 });
 
 connection.on("error", function(err) {
   /* Act on the event */
-  console.log(chalk.blue(err));
+  wifi.emit('error', err);
 });
 
 connection.on('close', function() {
   console.log('connection closed');
 });
+
+
+
+
+wifi.on('down', function() {
+  console.log('WiFi Выключен');
+});
+
+wifi.on('up', function() {
+  console.log('WiFi Включён');
+});
+
+wifi.on('error', function(err) {
+
+  if (err) console.log(chalk.blue(err));
+  console.log(chalk.blue('Ошибка при подключении к роутеру'));
+  
+});
+
 
 function init() {
 
@@ -41,8 +58,9 @@ function init() {
     var message = (process.argv[2] == 'up') ? chalk.green.bold("Включить интерфейс WiFi...") : chalk.yellow.bold("Выключить интерфейс WiFi...");
     console.log(message);
     connection.connect(params);
-    
+
   } else {
+    wifi.emit('error');
     console.log("Задайте параметры корректно");
   }
 }
---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {
+  "connection": {
+    host: '192.168.1.1',
+    port: 23,
+    username: "admin",
+    password: "admin",
+    shellPrompt: '#',
+    timeout: 3500,
+    debug: true
+  }
+}
+
+module.exports = config;
