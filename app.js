482d451532bf5de7bdea9f626f66b2b37d3c2bce
Dragan <iliya5376@mail.com>
2017-08-03 21:05:48 +0300

    refactor(app.js) add class to wifi entity

--- app.js
+++ app.js
@@ -2,67 +2,84 @@ var events = require('events');
 var chalk = require('chalk');
 var telnet = require('telnet-client');
 var config = require('./config');
-var connection = new telnet();
-var wifi = new events.EventEmitter();
-
-var params = config.connection;
-
-connection.on('ready', function(prompt) {
-  var cmd = 'ifconfig ra0 ' + process.argv[2];
-  connection.exec(cmd, function(err, response) {
-    //if (err) wifi.emit('error');
-
-    //if (response) wifi.emit(process.argv[2]);
-    wifi.emit(process.argv[2]);
-  });
-});
-
-connection.on('timeout', function() {
-  //console.log('socket timeout!')
-  connection.end();
-});
-
-connection.on("error", function(err) {
-  /* Act on the event */
-  wifi.emit('error', err);
-});
-
-connection.on('close', function() {
-  console.log('connection closed');
-});
+//var wifi = new events.EventEmitter();
 
 
 
+class WIFI extends events.EventEmitter {
 
-wifi.on('down', function() {
-  console.log('WiFi Выключен');
-});
+  constructor(params) {
+    super();
+    this.params = params;
+    this.connection = new telnet();
+    var self = this;
 
-wifi.on('up', function() {
-  console.log('WiFi Включён');
-});
+    this.connection.on('ready', function(prompt) {
 
-wifi.on('error', function(err) {
+      var cmd = 'ifconfig ra0 ' + self.cmd;
+      this.exec(cmd, function(err, response) {
 
-  if (err) console.log(chalk.blue(err));
-  console.log(chalk.blue('Ошибка при подключении к роутеру'));
-  
-});
+        //if (err) wifi.emit('error');
+        if (err) console.log(err);
+        self.emit(self.cmd);
+      });
+    });
+
+    this.connection.on('timeout', function() {
+      //console.log('socket timeout!')
+      this.end();
+    });
+
+    this.connection.on("error", function(err) {
+      /* Act on the event */
+      self.emit('error', err);
+    });
+
+    this.connection.on('close', function() {
+      //console.log('connection closed');
+      console.log(chalk.blue('Подключение окончено...'));
+    });
+
+    this.on('down', function() {
+      console.log(chalk.yellow('WiFi Выключен...ОК'));
+    });
+
+    this.on('up', function() {
+      console.log(chalk.green('WiFi Включён...ОК'));
+    });
+
+    this.on('error', function(err) {
+
+      if (err) console.log(chalk.blue(err));
+      console.log(chalk.blue('Ошибка при подключении к роутеру...'));
+
+    });
 
 
-function init() {
 
-  var validParams = ["up", "down"];
-  if (validParams.includes(process.argv[2])) {
+  }
 
-    var message = (process.argv[2] == 'up') ? chalk.green.bold("Включить интерфейс WiFi...") : chalk.yellow.bold("Выключить интерфейс WiFi...");
-    console.log(message);
-    connection.connect(params);
+  change(cmd) {
 
-  } else {
-    wifi.emit('error');
-    console.log("Задайте параметры корректно");
+    var validParams = ["up", "down"];
+    if (validParams.includes(cmd)) {
+
+      var message = (cmd == 'up') ?
+        chalk.green("Включить интерфейс WiFi...") :
+        chalk.yellow("Выключить интерфейс WiFi...");
+
+      console.log(message);
+      this.cmd = cmd;
+      this.connection.connect(this.params);
+
+    } else {
+      this.emit('error');
+      console.log("Задайте параметры корректно");
+    }
   }
 }
 
-init();
+var params = config.connection;
+var wifi = new WIFI(params);
+
+wifi.change(process.argv[2]);
