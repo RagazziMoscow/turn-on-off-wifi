2dc0294e81127540fcc01b80c617d4e01265d361
Ilya <iliya5376@mail.com>
2017-09-19 21:06:26 +0300

    refactor(app.js) params are moved in app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var WIFI = require('./lib');
-var config = require('./config');
 
-
-var params = config.connection;
+var params = {
+  host: '192.168.1.1',
+  port: 23,
+  username: "admin",
+  password: "admin",
+  shellPrompt: '#',
+  timeout: 2500,
+  debug: true
+};
 var wifi = new WIFI(params);
 
 wifi.change(process.argv[2]);
