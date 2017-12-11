004ff3e08b0fdb3c91de17c5764095a1269be4ad
Dragan <iliya5376@mail.com>
2017-12-12 00:42:31 +0300

    feature(app.js) add config command line interface

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
+var chalk = require('chalk');
 var WIFI = require('./lib');
+var fs = require('fs');
 
-var params = {
-  host: '192.168.1.1',
-  port: 23,
-  username: "admin",
-  password: "admin",
-  shellPrompt: '#',
-  timeout: 2500,
-  debug: true
-};
-var wifi = new WIFI(params);
+const minimist = require('minimist');
+const params = JSON.parse(fs.readFileSync('./config/index.json'));
 
-wifi.change(process.argv[2]);
+const args = minimist(process.argv.slice(2));
+const connectCondition = (args['_'][0] == 'down' || args['_'][0] == 'up');
+
+if (connectCondition) {
+  var wifi = new WIFI(params);
+  wifi.change(args['_'][0]);
+} else {
+
+  // config
+  const host = args['host'] || params['host'];
+  const port = args['port'] || params['port'];
+  const username = args['username'] || params['username'];
+  const password = args['password'] || params['password'];
+  const shellPrompt = '#';
+  const timeout = 2500;
+  const debug = true;
+  const newConfig = {
+    host,
+    port,
+    username,
+    password,
+    shellPrompt,
+    timeout,
+    debug
+  };
+
+  fs.writeFileSync('./config/index.json', JSON.stringify(newConfig));
+  console.log(chalk.green('Конфигурация изменена...ОК'));
+}
