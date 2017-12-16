bdcbdad0300fd45b5c1c9ec90035c1209664d180
Dragan <iliya5376@mail.com>
2017-12-16 16:28:45 +0300

    rafactor(app.js) add yargs and configurate options

--- bin/app.js
+++ bin/app.js
@@ -1,50 +1,65 @@
 #!/usr/bin/env node
+
 var chalk = require('chalk');
 var path = require('path');
 var WIFI = require('./../lib');
 var fs = require('fs');
 
-const minimist = require('minimist');
-const params = JSON.parse(fs.readFileSync(path.join(__dirname, '../config/index.json')));
-
-const args = minimist(process.argv.slice(2));
-const connectCondition = (args['_'][0] == 'down' || args['_'][0] == 'up');
-
-if (connectCondition) {
-  var wifi = new WIFI(params);
-  wifi.change(args['_'][0]);
-} else {
-
-  // config
-  const host = args['host'] || params['host'];
-  const port = args['port'] || params['port'];
-  const username = args['username'] || params['username'];
-  const password = args['password'] || params['password'];
-  const shellPrompt = '#';
-  const timeout = 2500;
-  const debug = true;
-  const newConfig = {
-    host,
-    port,
-    username,
-    password,
-    shellPrompt,
-    timeout,
-    debug
-  };
-
-  const helpCondition = (Object.keys(args).includes('help'));
-
-  if (helpCondition) {
-    console.log('Использование: WIFI [up | down] [--port <number>] [--host <host_value>] [--username <user>] [--password <pass>]');
-    console.log('up       Включить WIFI');
-    console.log('down     Выключить WIFI');
-    console.log('port     Задать порт роутера');
-    console.log('host     Задать адрес роутера');
-    console.log('username Задать пользователя для подключения');
-    console.log('password Задать пароль для подключения');
-  } else {
-    fs.writeFileSync(path.join(__dirname, '../config/index.json'), JSON.stringify(newConfig));
+const argv = require('yargs')
+  .command('up', 'turn on wifi')
+  .command('down', 'turn off wifi')
+  .command('config', 'write config options', (yargs) => {
+    yargs.positional('port', {
+      describe: 'a unique number for the port',
+      type: 'number',
+      default: 23
+    });
+    yargs.positional('host', {
+      describe: 'a unique identifier for the host',
+      type: 'string',
+      default: '192.168.1.1'
+    });
+    yargs.positional('username', {
+      describe: 'string containing username',
+      type: 'string',
+      default: 'admin'
+    });
+    yargs.positional('password', {
+      describe: 'string containing password',
+      type: 'string',
+      default: 'admin'
+    });
+  }, (argv) => {
+    const configFilePath = path.join(__dirname, '../config/index.json');
+    const host = argv.host;
+    const port = argv.port;
+    const username = argv.username;
+    const password = argv.password;
+    const shellPrompt = '#';
+    const timeout = 2500;
+    const debug = true;
+    const newConfig = {
+      host,
+      port,
+      username,
+      password,
+      shellPrompt,
+      timeout,
+      debug
+    };
+    fs.writeFileSync(configFilePath, JSON.stringify(newConfig));
     console.log(chalk.green('Конфигурация изменена...ОК'));
-  }
+  })
+  .help()
+  .argv;
+
+
+
+const commands = argv['_'];
+const connectCondition = (commands.includes('up') || commands.includes('down'));
+if (connectCondition) {
+  const configFilePath = path.join(__dirname, '../config/index.json');
+  const params = JSON.parse(fs.readFileSync(configFilePath));
+  var wifi = new WIFI(params);
+  wifi.change(argv['_'][0]);
 }
